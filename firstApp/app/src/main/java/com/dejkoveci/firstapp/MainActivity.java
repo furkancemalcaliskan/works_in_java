34902d8147795b1e084a98795a986715f189bf41
Furkan Cemal Çalışkan <furkanc.caliskan@icloud.com>
2022-11-15 09:26:23 +0300

    Added changeImage

--- firstApp/app/src/main/java/com/dejkoveci/firstapp/MainActivity.java
+++ firstApp/app/src/main/java/com/dejkoveci/firstapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.dejkoveci.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void changeImage(View view) {
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.dejkoveci_bw);
+    }
+
 }
