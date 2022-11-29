34daa1a0fb1046b00c58f2da6679caf91fa6130d
Furkan Cemal Çalışkan <furkanc.caliskan@icloud.com>
2022-11-29 23:45:12 +0300

    Removed Android Files and Created dataTypesNew

---
+++ dataTypesNew/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int sayi = 12;
+        sayi = 1333333;
+
+        String sehir = "Ankara";
+        char karakter = 's';
+
+        boolean dogruMu = true;
+    }
+}
