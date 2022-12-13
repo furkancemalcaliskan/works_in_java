c405037df72b8799e2a3708bf754c1205483d65b
Furkan Cemal Çalışkan <furkanc.caliskan@icloud.com>
2022-12-13 15:05:33 +0300

    Created inheritanceNew

---
+++ inheritanceNew/src/EmployeesManager.java
@@ -0,0 +1,6 @@
+public class EmployeesManager extends PersonManager{
+
+    public void BestEmployee() {
+        System.out.println("Ayin elemani getirildi");
+    }
+}
---
+++ inheritanceNew/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Customer customer = new Customer();
+        Employees employee = new Employees();
+
+        EmployeesManager employeesManager = new EmployeesManager();
+        CustomerManager customerManager = new CustomerManager();
+
+    }
+}
